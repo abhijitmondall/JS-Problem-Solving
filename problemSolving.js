633afd17e92970e9770db4374518a576d20699d1
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-07-27 02:53:49 +0600

    findSecondSmallest

--- problemSolving.js
+++ problemSolving.js
@@ -141,3 +141,9 @@ const romanToInt = function (romanNumeral) {
 };
 
 // console.log(romanToInt("IX"));
+
+//
+// Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
+const findSecondSmallest = (arr) => +arr.sort().slice(1, 2).join("");
+
+// console.log(findSecondSmallest([5, 2, 8, 1, 3, 7]));

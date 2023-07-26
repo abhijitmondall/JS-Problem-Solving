b73fdee235eae56181d436782ff3dbee479f1e6f
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-07-27 01:37:18 +0600

    JS-Problem-Solving

---
+++ problemSolving.js
@@ -0,0 +1,54 @@
+//
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+const stringReverse = function (str) {
+  // Input will be string
+
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  // Return the reverse of the string
+  return reversedStr;
+};
+
+// console.log(stringReverse("Hello World"));
+
+//
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+const sumOfPositiveNum = (arr) =>
+  arr.reduce((acc, val) => acc + (val >= 0 && val), 0);
+
+// console.log(sumOfPositiveNum([2, -5, 10, -3, 7]));
+
+//
+//Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
+const mostFrequentEl = function (arr) {
+  // Input will be an Array
+  // Create new Map
+  const map = new Map();
+
+  let result = 0;
+  let frequentKey;
+
+  // Loop through the input array and set the Element in the new Map with it's value
+  arr.map((el) => map.set(el, map.has(el) ? map.get(el) + 1 : 1));
+
+  // Loop through the new Map
+  for (let [key, val] of map) {
+    // check the current val of new Map is greater than the result variable value
+    if (val > result) {
+      // Update Result variable
+      result = val;
+
+      // Update frequent Element
+      frequentKey = key;
+    }
+  }
+
+  // Return Most frequent element
+  return frequentKey;
+};
+
+console.log(mostFrequentEl([3, 5, 2, 5, 3, 3, 1, 4, 5]));
